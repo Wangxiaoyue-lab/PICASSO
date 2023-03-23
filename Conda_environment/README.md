e73109efd31cdb003d22907630ceb95cfcee70c5
Moloch0 <luoliheng@nwpu.mail.edu.cn>
2023-03-23 15:30:07 +0800

    1

---
+++ Conda_environment/README.md
@@ -0,0 +1,13 @@
+*Scripts written in R will be executed in r1 and python in py1*
+
+# to copy
+
+```shell
+mamba env export -n r1 > r1.yaml
+```
+
+# to creat
+
+```shell
+mamba env create -n r1 -f r1.yaml
+```
--- Sample_pipline/qc.R
+++ Sample_pipline/qc.R
@@ -1,12 +1,26 @@
+# Load in the utils.R script to use the functions defined in it
 source("/public/home/luoliheng/SINGLE/R/utils.R", chdir = TRUE)
 
+# Load in the tidyverse package and suppress the package startup messages
 suppressPackageStartupMessages(require(tidyverse))
+# Load in the Seurat package and suppress the package startup messages
 suppressPackageStartupMessages(require(Seurat))
+
+# Define the working directory where the data is stored
 work_dir <- "/public/home/luoliheng/yujia/analysis/gan/basic-analysis/qc/"
+# Define the project name
 project_name <- "LIVER"
+# Define the species
 species <- "mm"
+# Read in the data
 all_data <- readRDS(paste0(work_dir, "gan_qc.rds"))
 
+# Find the markers for each cluster
+log_info("Finding markers...")
 marker_genes <- find_markers(all_data, all = FALSE, ident = NULL, loop_var = 0:3, ident.1 = "HF", group.by = "type", only.pos = FALSE)
+log_info("Finding markers done")
 
+# Save the markers to a file
+log_info("Saving markers to file...")
 save_file(data = marker_genes, fun = write.csv, name_string = "markers_sz", row.names = FALSE, col.names = TRUE)
+log_info("Saving markers to file done")
