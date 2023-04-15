8479694182f0e7328d024574768815f49e0fc850
Moloch0 <luoliheng@nwpu.mail.edu.cn>
2023-04-15 22:41:50 +0800

    deleted

--- Sample_pipline/R_qc_normal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -J test
-#SBATCH -p normal
-#SBATCH -n 2
-#SBATCH -N 1
-#SBATCH --mem 200g
-#SBATCH --exclusive
-~/miniconda3/envs/r1/bin/Rscript /public/home/luoliheng/SINGLE/Sample_pipline/qc.R
