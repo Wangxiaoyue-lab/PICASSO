344dc84ae828231c42373a6e32a4d8fcb9978436
Moloch0 <luoliheng@nwpu.mail.edu.cn>
2023-04-18 17:51:45 +0800

    deleted:    sc_omics/scrnaseq/pseudotime/velocity/scvelo/scvelo.py

--- utils/parallel/parallel_in_cores.sh
+++ utils/parallel/parallel_in_cores.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #SBATCH --nodes 4
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 21
@@ -5,7 +6,7 @@
 #SBATCH --exclusive
 #SBATCH -o /public/home/caojun/project/OSCAR/small_bench_20230316/script/2_other_data_to_seurat/log.out/oscar_scenic.out
 #SBATCH -e /public/home/caojun/project/OSCAR/small_bench_20230316/script/2_other_data_to_seurat/log.out/oscar_scenic.err
- 
+
 echo 'start'
 srun --exclusive <script> &
 echo 'start   1'
