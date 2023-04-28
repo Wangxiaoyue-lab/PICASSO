7f08d8a9c8c42c2b4e2e901c78da44a4d8c86c52
zhangxv <zhangxv2049@outlook.com>
2023-04-28 16:11:27 +0800

    add salmon

---
+++ sc_omics/scrnaseq/upstream/upstream/salmon/README.read
@@ -0,0 +1,9 @@
+#使用salmon进行计量前需要先构建index
+
+#可以构建三种不同的index，
+#其大小、计量时所需资源和计量准确度各有不同，
+#具体信息可以参考作者主页：https://github.com/COMBINE-lab/salmon
+
+#本脚本构建的index类型为SAF genome index，
+#构建该index前需要现在gencode下载相应的genome和transcript文件，
+#步骤可以参考：https://combine-lab.github.io/alevin-tutorial/2019/selective-alignment/
---
+++ sc_omics/scrnaseq/upstream/upstream/salmon/salmon_create_index.sh
@@ -0,0 +1,19 @@
+#create salmon index
+module purge
+module load apps/salmon/1.10.0-x86_64
+
+genome="GRCm38.primary_assembly.genome.fa.gz"
+transcript="gencode.vM23.transcripts.fa.gz"
+decoy="decoys.txt"
+gentrome="gentrome.fa.gz"
+
+#Preparing
+#create decoys file
+grep "^>" <(gunzip -c ${genome}) | cut -d " " -f 1 > ${decoy}
+sed -i.bak -e 's/>//g' ${decoy}
+#create concatenated transcriptome and genome reference file
+cat ${transcript} ${genome} > ${gentrome}
+
+#Salmon Indexing
+salmon index -t ${gentrome} -d ${decoy} -p 12 -i salmon_index --gencode
+
