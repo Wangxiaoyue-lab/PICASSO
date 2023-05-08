3c1cf9b0b8e962756bcb724c6b49f97f62ccd233
caodudu <caojundudu@qq.com>
2023-05-08 11:07:20 +0800

    new file:   sc_omics/scrnaseq/cnv/CNV.R

---
+++ sc_omics/scrnaseq/cnv/CNV.R
@@ -0,0 +1,14 @@
+sc_cnv <- function() {
+    sc_cnv_infercnv <- function() {
+
+    }
+    sc_cnv_copykat <- function() {
+
+    }
+    sc_cnv_casper <- function() {
+        # CaSpER
+    }
+    sc_cnv_honeybadger <- function() {
+
+    }
+}
