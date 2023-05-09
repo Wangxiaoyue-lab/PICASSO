62af26e779e88da02a89e01c437e2222b040ecd1
caodudu <caojundudu@qq.com>
2023-05-09 23:42:38 +0800

    modified:   sc_omics/scrnaseq/cnv/CNV.R

--- sc_omics/scrnaseq/chat/chat.R
+++ sc_omics/scrnaseq/chat/chat.R
@@ -5,6 +5,7 @@ sc_chat <- function(...) {
     sc_chat_cellchat <- function(...) {
         next
     }
+    # https://github.com/pkuerten/single_cell_interaction.github.io/blob/main/index.md
     sc_chat_nichenet <- function(...) {
         next
     }
