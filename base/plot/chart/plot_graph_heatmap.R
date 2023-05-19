5421d0bbbdeb133bf581f2d7e5fa3813a56de2ff
caodudu <caojundudu@qq.com>
2023-05-19 17:09:13 +0800

    modified

--- base/plot/chart/plot_graph_heatmap.R
+++ base/plot/chart/plot_graph_heatmap.R
@@ -61,6 +61,60 @@ plot_rearrange <- function(df, group_df, seed = 1) {
 }
 
 
+#' Reorder hierarchical clustering by the specified method
+#'
+#' This function reorders hierarchical clustering of a matrix based on the specified method. It supports several methods, such as "raw", "manual", "row_select", "col_names", and "eigenvalue".
+#'
+#' @param mat a matrix or data frame for clustering
+#' @param method character string specifying the reordering method, one of "raw", "manual", "row_select", "col_names", "eigenvalue"
+#' @param manual optional vector specifying the manual order for rows, only used if method is "manual"
+#' @param row_select optional character string specifying a column in mat for row ordering, only used if method is "row_select"
+#' @return an object of class `hclust` with reordered clustering
+#' @import dendextend
+#' @import assertthat
+#' @export
+#' @examples
+#' mat <- matrix(rnorm(100), nrow = 10)
+#' ordered_hclust <- hclust_order(mat, method = "raw")
+hclust_order <- function(mat, method, manual=NULL, row_select=NULL) {
+        library(dendextend)
+        assertthat::assert_that(method %in% c("raw","manual","row_select","col_names","eigenvalue"))
+        mat_t <- mat %>%
+                t() %>%
+                as.data.frame()
+        hclust_mat <- mat_t %>%
+                dist() %>%
+                hclust()
+        if (method == "raw"){
+                return(hclust_mat)
+        } else if (method == "manual") {
+                hclust_manual <- reorder(as.dendrogram(hclust_mat),
+                        wts = order(match(manual, rownames(mat_t)))
+                ) %>%
+                        as.hclust()
+                return(hclust_manual)
+        } else if (method == "row_select") {
+                hclust_row_select <- reorder(as.dendrogram(hclust_mat),
+                        wts = mat_t[, row_select]
+                ) %>%
+                        as.hclust()
+                return(hclust_row_select)
+        } else if (method == "col_names") {
+                hclust_col_names <- hclust_mat %>%
+                        as.dendrogram() %>%
+                        sort() %>%
+                        as.hclust()
+                return(hclust_col_names)
+        } else if (method == "eigenvalue") {
+                hclust_eigenvalue <- reorder(as.dendrogram(hclust_mat), 
+                                wts = svd(mat)$v[, 1]) %>% 
+                        as.hclust()
+                return(hclust_eigenvalue)
+        }
+}
+
+
+
 #' Plot a heatmap
 #'
 #' This function takes a data matrix and optional parameters to plot a heatmap using the pheatmap package. The function allows for customization of the color palette, row and column annotations, clustering options, and more.
@@ -150,6 +204,80 @@ plot_heatmap <- function(data,
         }
 }
 
+
+
+
+#' Add flag to a pheatmap object
+#'
+#' This function adds flag to a pheatmap object for the given kept labels. The repel.degree parameter controls how much space is allocated for repelling labels.
+#' @param pheatmap pheatmap object to add flag to
+#' @param kept.labels list of labels to add flag for
+#' @param repel.degree number within [0, 1], controls label repulsion. 0: spread out labels over existing range of kept labels. 1: spread out labels over the full y-axis
+#' @return updated heatmap object with added flags
+#' @import ggplot2
+#' @import grid
+#' @export
+#' @examples
+#' plot_add_flag(pheatmap, c("label1", "label2"), 0.5)
+plot_add_flag <- function(pheatmap,  
+                     kept.labels,
+                     repel.degree) {
+        library(ggplot2)
+        library(grid)
+
+        heatmap <- pheatmap$gtable
+        new.label <- heatmap$grobs[[which(heatmap$layout$name == "row_names")]]
+        new.label$label <- ifelse(new.label$label %in% kept.labels,
+                new.label$label, ""
+        )
+        repelled.y <- function(d, d.select, k = repel.degree) {
+                strip.npc <- function(dd) {
+                        if (!"unit.arithmetic" %in% class(dd)) {
+                                return(as.numeric(dd))
+                        }
+                        d1 <- strip.npc(dd$arg1)
+                        d2 <- strip.npc(dd$arg2)
+                        fn <- dd$fname
+                        return(lazyeval::lazy_eval(paste(d1, fn, d2)))
+                        full.range <- sapply(seq_along(d), function(i) strip.npc(d[i]))
+                        selected.range <- sapply(seq_along(d[d.select]), function(i) strip.npc(d[d.select][i]))
+                        return(unit(
+                                seq(
+                                        from = max(selected.range) + k * (max(full.range) - max(selected.range)),
+                                        to = min(selected.range) - k * (min(selected.range) - min(full.range)),
+                                        length.out = sum(d.select)
+                                ),
+                                "npc"
+                        ))
+                }
+                new.y.positions <- repelled.y(new.label$y,
+                        d.select = new.label$label != ""
+                )
+                new.flag <- segmentsGrob(
+                        x0 = new.label$x,
+                        x1 = new.label$x + unit(0.15, "npc"),
+                        y0 = new.label$y[new.label$label != ""],
+                        y1 = new.y.positions
+                )
+                new.label$x <- new.label$x + unit(0.2, "npc")
+                new.label$y[new.label$label != ""] <- new.y.positions
+                heatmap <- gtable::gtable_add_grob(
+                        x = heatmap,
+                        grobs = new.flag,
+                        t = 4,
+                        l = 4
+                )
+                heatmap$grobs[[which(heatmap$layout$name == "row_names")]] <- new.label
+                grid.newpage()
+                grid.draw(heatmap)
+                invisible(heatmap)
+        }
+}
+
+
+
+
+
 # 长数据热图
 # stemangiola/tidyHeatmap
 # https://cran.r-project.org/web/packages/tidyHeatmap/vignettes/introduction.html
@@ -193,6 +321,9 @@ plot_heatmap <- function(data,
 # add_point(activation) |>
 # add_bar(size) |>
 # add_line(age)|>
+
+# 增加标记层
+# layer_point(pval < 0.05)|>
 
 # 拼图
 # library(patchwork)
