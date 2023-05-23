2236f4a2c23bf0eae163eca165cff53f122f4e7f
caodudu <caojundudu@qq.com>
2023-05-23 15:56:20 +0800

    cellranger

--- sc_omics/scrnaseq/upstream/cellranger.sh
+++ sc_omics/scrnaseq/upstream/cellranger.sh
@@ -9,7 +9,7 @@
 # software
 installed_cellranger=//
 
-installed_multiqc=///
+
 
 
 # define refrence genome
@@ -57,13 +57,5 @@ do
 done
 
 
-# summarise the result of cellranger
-mkdir ${project}/${task}/output/cellranger_multiqc
-cd ${project}/${task}/output/cellranger_multiqc
-ls ${project}/${task}/output/cellranger_result | while read sample;
-do
-ln -s ${project}/${task}/output/cellranger_result/${sample}/outs/web_summary.html \
-    ${project}/${task}/output/cellranger_multiqc/${sample}_web_summary.html
-done
-multiqc .
+
 
