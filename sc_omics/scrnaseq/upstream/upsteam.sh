7479df478eac3c3429e259836ffde1cf2b984235
caodudu <caojundudu@qq.com>
2023-06-13 23:11:50 +0800

    new file

---
+++ sc_omics/scrnaseq/upstream/upsteam.sh
@@ -0,0 +1,17 @@
+#zUMIs [Parekh et al., 2018]
+
+
+#alevin [Srivastava et al., 2019]
+
+
+#RainDrop [Niebler et al., 2020]
+
+
+#kallisto|bustools [Melsted et al., 2021]
+
+
+#STARsolo [Kaminow et al., 2021]
+
+
+#alevin-fry [He et al., 2022] 
+
